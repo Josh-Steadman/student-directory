81e69ed9b0cbcdb530dc386604406ed196cbf793
joshSteadman <josh.steadman15@gmail.com>
2020-07-27 14:45:30 +0100

    student(name) andstudent(cohort)

--- directory.rb
+++ directory.rb
@@ -2,17 +2,17 @@
 
 # array of the students
 students = [
-  "Dr Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Crouger",
-  "The Joker",
-  "Joffery Baratheon",
-  "Norman Bates",
+  {name: "Dr Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Crouger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffery Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 # Get list of students
 def print_header
@@ -20,7 +20,7 @@ def print_header
   puts "---------"
 end
 def print_names(names)
-names.each { |student| puts student }
+names.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 def print_footer(names)
 print "Overall, we have #{names.count} great students"
